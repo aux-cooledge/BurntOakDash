f0942898362fc099458d90f4061c463fda5d72b8
aux-cooledge <50361829+aux-cooledge@users.noreply.github.com>
2019-05-15 10:20:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,69 @@
+import math
+
+import pandas as pd
+import flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dateutil.parser
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from sfManager import sf_Manager
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+app.config.suppress_callback_exceptions = True
 
-app.layout = html.Div([
-    dcc.Input(id='my-id', value='initial value', type='text'),
-    html.Div(id='my-div')
-])
+sf_manager = sf_Manager()
 
-
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+millnames = ["", " K", " M", " B", " T"] # used to convert numbers
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# return html Table with dataframe values  
+def df_to_table(df):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in df.columns])] +
+        
+        # Body
+        [
+            html.Tr(
+                [
+                    html.Td(df.iloc[i][col])
+                    for col in df.columns
+                ]
+            )
+            for i in range(len(df))
+        ]
+    )
+
+
+#returns most significant part of a number
+def millify(n):
+    n = float(n)
+    millidx = max(
+        0,
+        min(
+            len(millnames) - 1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))
+        ),
+    )
+
+    return "{:.0f}{}".format(n / 10 ** (3 * millidx), millnames[millidx])
+
+
+#returns top indicator div
+def indicator(color, text, id_value):
+    return html.Div(
+        [
+            
+            html.P(
+                text,
+                className="twelve columns indicator_text"
+            ),
+            html.P(
+                id = id_value,
+                className="indicator_value"
+            ),
+        ],
+        className="four columns indicator",
+        
+    )
